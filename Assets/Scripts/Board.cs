649755714512f6faec24e297f4bc7fc511207bd4
Sean Francis-Lyon <seanflyon@gmail.com>
2018-08-11 13:37:30 -0700

    Random tile heights

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -8,13 +8,19 @@ public class Board : MonoBehaviour {
 
     public Transform TileContainer;
 
-    List<List<GameObject>> tiles = new List<List<GameObject>>();
+    List<List<Tile>> Tiles = new List<List<Tile>>();
 
 	void Start () {
 		for (int i = 0; i < 15; i++) {
+            Tiles.Add(new List<Tile>());
             for (int j = 0; j < 15; j++) {
-                GameObject tile = GameObject.Instantiate(TilePrefab, new Vector3((j - i)*0.42f, (i + j) * 0.21f, (i + j) * 0.21f), Quaternion.identity, TileContainer);
-                tile.name = "Tile( " + i + ", " + j + ")";
+                GameObject tileObj = GameObject.Instantiate(TilePrefab, TileContainer, false);
+                tileObj.transform.localPosition = new Vector3((j - i) * 0.42f, (i + j) * 0.21f, (i + j) * 0.21f);
+                //GameObject tileObj = GameObject.Instantiate(TilePrefab, new Vector3((j - i)*0.42f, (i + j) * 0.21f, (i + j) * 0.21f), Quaternion.identity, TileContainer);
+                tileObj.name = "Tile( " + i + ", " + j + ")";
+                Tile tile = tileObj.GetComponent<Tile>() as Tile;
+                tile.SetHeight(Random.Range(-0.1f, 0.1f));
+                Tiles[i].Add(tile);
             }
         }
 	}
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -4,5 +4,9 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour {
 
+    public Transform container;
 
+    public void SetHeight(float height) {
+        container.localPosition = new Vector3(0f, height, 0f);
+    }
 }
