db18900a16163c699bf52de23961bf94a3cb3d34
Andrew Blevins <andrew.blevins@recursionpharma.com>
2018-08-12 10:26:39 -0600

    scene changes

--- Assets/Scripts/ResourcesPanel.cs
+++ Assets/Scripts/ResourcesPanel.cs
@@ -9,10 +9,13 @@ public class ResourcesPanel : MonoBehaviour
     private float gold = 10f;
     private float iron = 50f;
     private float coal = 100f;
+    private float food = 100f;
 
     public Text ironText;
     public Text coalText;
     public Text goldText;
+    public Text foodText;
+
     public static ResourcesPanel Instance { get; private set; }
 
     void Awake()
@@ -32,9 +35,10 @@ public class ResourcesPanel : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        UpdateIron(100);
-        UpdateCoal(40);
-        UpdateGold(100);
+        UpdateIron(100f);
+        UpdateCoal(40f);
+        UpdateGold(100f);
+        UpdateFood(100f);
 
     }
 
@@ -78,6 +82,12 @@ public class ResourcesPanel : MonoBehaviour
 
     }
 
+    public void UpdateFood(float amount)
+    {
+        food += amount;
+        UpdateFoodText(coal);
+
+    }
     private void UpdateIronText(float t)
     {
         ironText.text = t.ToString();
@@ -92,5 +102,8 @@ public class ResourcesPanel : MonoBehaviour
     {
         goldText.text = t.ToString();
     }
-
+    private void UpdateFoodText(float t)
+    {
+        foodText.text = t.ToString();
+    }
 }
